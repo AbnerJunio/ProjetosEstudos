d52e3adbbc3c708d30239b269dcfdf077aab273b
Abner <uis9215@interno.usiminas.com>
2023-08-04 08:43:40 -0300

    Estudos C#

---
+++ Escopo-e-lógica/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Escopo_e_lógica;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~OS COMANDOS ABAIXO SÃO SOMENTE PARA BASE DE ESTUDOS~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine();
+        Console.WriteLine("Pressione alguma tecla para continuar\t");
+        Console.ReadKey();
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        
+        int[] numbers = { 4, 8, 15, 16, 23, 42 };
+        bool found = false;
+        int total = 0;
+
+        foreach (int number in numbers)
+        {
+            total += number;
+            if (number == 42)
+            {
+                found = true;
+            }
+        }
+
+        if (found)
+        {
+            Console.WriteLine("Set contains 42");
+        }
+        Console.WriteLine($"Total: {total}");
+    }
+}
+
---
+++ Switch-Case/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Switch_Case;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~OS COMANDOS ABAIXO SÃO SOMENTE PARA BASE DE ESTUDOS~~~~~~~~~~~~~~~~~~~~~~\n");
+        Console.WriteLine("Pressione enter para continuar");
+        Console.ReadKey();
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+        Console.Write("Nome do funcionário: ");
+        string employeeName = Console.ReadLine().ToUpper();
+
+        string title = "";
+        Console.Write("Numero da patente <0 a 600>: ");
+        int employeeLevel = int.Parse(Console.ReadLine());
+        switch (employeeLevel)
+        {
+            case >= 400:
+                title = "Senior Manager";
+                break;
+            case >= 300:
+                title = "Manager";
+                break;
+            case >= 200:
+                title = "Senior Associate";
+                break;
+            case >= 100:
+                title = "Junior Associate";
+                break;
+            default:
+                title = "Associate";
+                break;
+        }
+        Console.WriteLine($"{employeeName}, {title}");
+
+        Console.WriteLine("\nPrograma para avaliar os componentes de uma SKU de produto.");
+        Console.WriteLine("Pressione alguma tecla para continuar");
+        Console.ReadKey();
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        //Formato do valor SKU : <product #>-<2-letter color code>-<size code>
+        string sku = "01-BL-L";
+        Console.WriteLine("Product - letter color code - size code: "+sku);
+
+        string[] product = sku.Split('-');
+
+        string type = "";
+        string color = "";
+        string size = "";
+
+        switch (product[0])
+        {
+            case "01":
+                type = "Sweat shirt"; break;
+            case "02":
+                type = "T-Shirt"; break;
+            case "03":
+                type = "Sweat pants"; break;
+            default:
+                type = "Other"; break;
+        }
+
+        switch (product[1])
+        {
+            case "BL":
+                color = "Black"; break;
+            case "MN":
+                color = "Maroon"; break;
+            default:
+                color = "White"; break;
+        }
+
+        switch (product[2])
+        {
+            case "S":
+                size = "Small"; break;
+            case "M":
+                size = "Medium"; break;
+            case "L":
+                size = "Large"; break;
+            default:
+                size = "One Size Fits All"; break;
+        }
+
+        Console.WriteLine($"Product: {size} {color} {type}");
+    }
+}
