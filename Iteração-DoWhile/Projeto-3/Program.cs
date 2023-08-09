954f7bad138097bfede149df4d52f09dab9d218d
Abner <uis9215@interno.usiminas.com>
2023-08-09 07:54:27 -0300

    Estudos C#

--- Iteração-DoWhile/Projeto-3/Program.cs
+++ Iteração-DoWhile/Projeto-3/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~OS COMANDOS ABAIXO SÃO SOMENTE PARA BASE DE ESTUDOS~~~~~~~~~~~~~~~~~~~~~~\n");
+        Console.WriteLine("Pressione enter para continuar");
+        Console.ReadKey();
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        
         string[] myStringss = new string[4] { "I like pizza", "I like roast chicken", "I like salad", "I like all three of the menu choices" };
         int periodLocation;
 
